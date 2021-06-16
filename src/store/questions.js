de34d4db90bfff7a6e160312a2d709c5ba1b4175
Nick Frost <nickfrost.us@gmail.com>
2021-06-16 14:32:32 -0400

    add more questions and answers

--- src/store/questions.js
+++ src/store/questions.js
@@ -61,10 +61,10 @@ export default [
         '(U.S. or national legislature)'
     ]),
     new Question('We elect a U.S. Senator for how many years?', ['six (6)']),
-    new Question('Who is one of your state\'s U.S. Senators now?', ['Answers will vary. Find your Senator here.'], 'https://www.senate.gov/senators/senators-contact.htm'),
+    new Question('Who is one of your state\'s U.S. Senators now?', ['Answers will vary, check here.'], 'https://www.senate.gov/senators/senators-contact.htm'),
     new Question('The House of Representatives has how many voting members?', ['four hundred thirty-five (435)']),
     new Question('We elect a U.S. Representative for how many years?', ['two (2)']),
-    new Question('Name your U.S. Representative.', ['Answers will vary. Find your Representative here.'], 'https://www.house.gov/representatives/find-your-representative'),
+    new Question('Name your U.S. Representative.', ['Answers will var,.cCheck here.'], 'https://www.house.gov/representatives/find-your-representative'),
     new Question('Who does a U.S. Senator represent?', ['all people of the state']),
     new Question('Why do some states have more Representatives than other states?', [
         '(because of) the state\'s population',
@@ -72,15 +72,204 @@ export default [
         '(because) some states have more people'
     ]),
     new Question('We elect a President for how many years?', ['four (4)']),
-    new Question('', ['']),
-    new Question('', ['']),
-    new Question('', ['']),
-    new Question('', ['']),
-    new Question('', ['']),
-    new Question('', ['']),
-    new Question('', ['']),
-    new Question('', ['']),
-    new Question('', ['']),
+    new Question('In what month do we vote for President?', ['November']),
+    new Question('What is the name of the President of the United States now?', ['Answers will vary, check here.'], 'https://www.usa.gov/presidents#item-37462'),
+    new Question('What is the name of the Vice President of the United States now?', ['Answers will vary, check here.'], 'https://www.usa.gov/presidents#item-37467'),
+    new Question('If the president can no longer serve, who becomes President?', ['the Vice President']),
+    new Question('If both  the President and the Vice President can no longer serve, who becomes President?', ['the Speaker of the House']),
+    new Question('Who is Commander in Chief of the military?', ['the President']),
+    new Question('Who signs bills to become laws?', ['the President']),
+    new Question('Who vetoes bills?', ['the President']),
+    new Question('What does the President\'s Cabinet do?', ['advises the President']),
+    new Question('What are two Cabinet-level positions?', [
+        'Secretary of Agriculture',
+        'Secretary of Commerce',
+        'Secretary of Defense',
+        'Secretary of Education',
+        'Secretary of Energy',
+        'Secretary of Health and Human Services',
+        'Secretary of Homeland Security',
+        'Secretary of Housing and Urban Development',
+        'Secretary of the Interior',
+        'Secretary of Labor',
+        'Secretary of State',
+        'Secretary of Transportation',
+        'Secretary of the Treasury',
+        'Secretary of Veterans Affairs',
+        'Attorney General',
+        'Vice President'
+    ]),
+    new Question('What does the judicial branch do?', [
+        'reviews laws',
+        'explains laws',
+        'resolves disputes (disagreements)',
+        'decides of a law goes against the Constitution'
+    ]),
+    new Question('What is the highest court in the United States?', ['the Supreme Court']),
+    new Question('How many justices are on the Supreme Court?', ['Answers will vary, check here.'], 'https://www.supremecourt.gov/about/justices.aspx'),
+    new Question('Who is the Chief Justice of the United States now?', ['Answers will vary, check here.'], 'https://www.supremecourt.gov/about/biographies.aspx'),
+    new Question('Under our Constitution, some powers belong to the federal government. What is one power of the federal government?', [
+        'to print money',
+        'to declare war',
+        'to create an army',
+        'to make treaties'
+    ]),
+    new Question('Under our Constitution, some powers belong to the states. What is one power of the states?', [
+        'provide schooling and education',
+        'provide protection (police)',
+        'provide safety (fire departments)',
+        'give a driver\'s license',
+        'approve zoning and land use'
+    ]),
+    new Question('Who is the Governor of your state now?', ['Answers will vary, check here.'], 'https://www.usa.gov/state-governor'),
+    new Question('What is the capital of your state?', ['Answers will vary, check here.'], 'https://simple.wikipedia.org/wiki/List_of_U.S._state_capitals'),
+    new Question('What are the two major political parties in the United States?', ['Democratic and Republican']),
+    new Question('What is the political party of the President now?', ['Answers will vary, check here.'], 'https://www.google.com/search?q=What+is+the+political+party+of+the+President+now?'),
+    new Question('What is the name of the Speaker of the House of Representatives now?', ['Answers will vary, check here.'], 'https://www.speaker.gov/'),
+    new Question('There are four amendments to the Constitution about who can vote. Describe one of them.', [
+        'Citizens eighteen (18) and older (can vote)',
+        'You don\'t have to pay (a poll tax) to vote.',
+        'Any citizen can vote. (Women and men can vote.)',
+        'A male citizen of any race (can vote).'
+    ]),
+    new Question('What is one responsibility that is only for United States citizens?', [
+        'serve on a jury',
+        'vote in a federal election'
+    ]),
+    new Question('Name one right only for United States citizens.', [
+        'vote in a federal election',
+        'run for federal election'
+    ]),
+    new Question('What are two rights of everyone living in the United States.', [
+        'freedom of expression',
+        'freedom of speech',
+        'freedom of assembly',
+        'freedom to petition the government',
+        'freedom of religion',
+        'the right to bear arms'
+    ]),
+    new Question('What do we show loyalty to when we say the Pledge of Allegiance?', [
+        'the United States',
+        'the flag'
+    ]),
+    new Question('What is one promise you can make when you become a United States citizen?', [
+        'give up loyalty to other countries',
+        'defend the Constitution and laws of the United States',
+        'obey the laws of the United States',
+        'serve in the U.S. military (if needed)',
+        'serve (do important work for) the nation (if needed)',
+        'be loyal to the United States'
+    ]),
+    new Question('How old do citizen have to be to vote for President?', ['eighteen (18) and older']),
+    new Question('What are two ways that Americans can participate in their democracy', [
+        'vote',
+        'join a political party',
+        'help with a campaign',
+        'join a civic group',
+        'join a community group',
+        'give an elected official your opinion on an issue',
+        'call Senators and Representatives',
+        'publicly support or oppose an issue or policy',
+        'run for office',
+        'write to a newspaper'
+    ]),
+    new Question('When is the last day you can send in federal income tax forms?', ['April 15']),
+    new Question('When must all men register for the Selective Service.', [
+        'at age eighteen (18)',
+        'between eighteen (18) and twenty-six (26)'
+    ]),
+    new Question('What is one reason colonists came to America?', [
+        'freedom',
+        'political liberty',
+        'religious freedom',
+        'economic opportunity',
+        'practice their religion',
+        'escape persecution'
+    ]),
+    new Question('Who lived in America before Europeans arrived?', [
+        'American Indians',
+        'Native Americans'
+    ]),
+    new Question('What group of people were taken to America and sold as slaves?', [
+        'Africans',
+        'people from Africa'
+    ]),
+    new Question('Why did the colonists fight the British?', [
+        'because of high taxes (taxation without representation)',
+        'because of the British army stayed in their houses (boarding, quartering)',
+        'because they didn\'t have self government'
+    ]),
+    new Question('Who wrote the Declaration of Independence?', ['(Thomas) Jefferson']),
+    new Question('When was the Declaration of Independence adopted?', ['July 4, 1776']),
+    new Question('There were 13 original states, name three.', [
+        'New Hampshire',
+        'Massachusetts',
+        'Rhode Island',
+        'Connecticut',
+        'New York',
+        'New Jersey',
+        'Pennsylvania',
+        'Delaware',
+        'Maryland',
+        'Virginia',
+        'North Carolina',
+        'South Carolina',
+        'Georgia'
+    ]),
+    new Question('What happened at the Constitutional Convention?', [
+        'The Constitution was written.',
+        'The Founding Fathers wrote the Constitution.'
+    ]),
+    new Question('When was the Constitution written?', ['1787']),
+    new Question('The Federalist Papers supported the passage of the U.S. Constitution. Name one of the writers.', [
+        '(James) Madison',
+        '(Alexander) Hamilton',
+        '(John) Jay',
+        'Publius'
+    ]),
+    new Question('What is on thing Benjamin Franklin is famous for?', [
+        'U.S. diplomat',
+        'oldest member of the Constitutional Convention',
+        'first Postmaster General of the United States',
+        'writer of "Poor Richard\'s Almanac"',
+        'started the first free libraries'
+    ]),
+    new Question('Who is the "Father of Our Country"?', ['(George) Washington']),
+    new Question('Who was the first President?', ['(George) Washington']),
+    new Question('What territory did the United States buy from France in 1803?', [
+        'the Louisiana Territory',
+        'Louisiana'
+    ]),
+    new Question('Name one war fought by the United States in the 1800s.', [
+        'War of 1812',
+        'Mexican-American War',
+        'Civil War',
+        'Spanish-American War'
+    ]),
+    new Question('Name the U.S. war between the North and the South.', [
+        'the Civil War',
+        'the War between the States'
+    ]),
+    new Question('Name one problem that led to the Civil War.', [
+        'slavery',
+        'economic reasons',
+        'state\'s rights'
+    ]),
+    new Question('What was one important think that Abraham Lincoln did?', [
+        'freed the slaves (Emancipation Proclamation)',
+        'saved (or preserved) the Union',
+        'led the United States during the Civil War'
+    ]),
+    new Question('What did the Emancipation Proclamation do?', [
+        'freed the slaves',
+        'freed slaves in the Confederacy',
+        'freed slaves in the Confederate states',
+        'freed slaves in the most Southern states'
+    ]),
+    new Question('What did Susan B. Anthony do?', [
+        'fought for women\'s rights',
+        'fought for civil rights'
+    ]),
     new Question('', ['']),
     new Question('', ['']),
     new Question('', ['']),
