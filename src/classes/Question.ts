eefbd61d6bc7b5d0433f3af8c6cc113c0b508185
Nick Frost <nickfrost.us@gmail.com>
2021-06-16 13:24:53 -0400

    slight modification to question class, added more questions to store array

--- src/classes/Question.ts
+++ src/classes/Question.ts
@@ -1,9 +1,11 @@
 export default class Question {
     text: String
     answers: Array<String>
+    link: String
 
-    constructor(text: String, answers: Array<String>) {
+    constructor(text: String, answers: Array<String>, link?: String | null) {
         this.text = text
         this.answers = answers
+        this.link = link
     }
 }
--- src/store/questions.js
+++ src/store/questions.js
@@ -1,5 +1,88 @@
 import Question from '../classes/Question'
 
 export default [
-    new Question('What is the supreme law of the land?', ['the Constitution'])
+    new Question('What is the supreme law of the land?', ['the Constitution']),
+    new Question('What does the Constitution do?', [
+        'sets up the government',
+        'defines the government',
+        'protects basic rights of Americans'
+    ]),
+    new Question('The idea of self-government is in the first three words of the Constitution. What are these words?', ['We the People']),
+    new Question('What is an amendment?', [
+        'a change (to the Constitution)',
+        'an addition (to the Constitution)'
+    ]),
+    new Question('What do we call the first ten amendments to the Constitution', ['the Bill of Rights']),
+    new Question('What is one right or freedom from the First Amendment', [
+        'speech',
+        'religion',
+        'assembly',
+        'press',
+        'petition the government',
+    ]),
+    new Question('How many amendments does the Constitution have?', ['twenty-seven (27)']),
+    new Question('What did the Declaration of Independence do?', [
+        'announced our independence (from Great Britain)',
+        'declared our independence (from Great Britain)',
+        'said that the United States is free (from Great Britain)',
+    ]),
+    new Question('What are two rights in the Declaration of Independence?', [
+        'life',
+        'liberty',
+        'pursuit of happiness'
+    ]),
+    new Question('What is freedom of religion?', ['You can practice any religion, or not practice a religion.']),
+    new Question('What is the economic system in the United States?', [
+        'capitalist economy',
+        'market economy'
+    ]),
+    new Question('What is the "rule of law"?', [
+        'Everyone must follow the law.',
+        'Leaders must obey the law.',
+        'Government must obey the law.',
+        'No one is above the law.'
+    ]),
+    new Question('Name one branch or part of the government.', [
+        'Congress',
+        'Legislative',
+        'President',
+        'Executive',
+        'the Courts',
+        'Judicial'
+    ]),
+    new Question('What stops one branch of government from becoming too powerful?', [
+        'checks and balances',
+        'separation of powers'
+    ]),
+    new Question('Who is in charge of the executive branch?', ['the President']),
+    new Question('Who makes federal laws?', [
+        'Congress',
+        'Senate and House (of Representative)',
+        '(U.S. or national legislature)'
+    ]),
+    new Question('We elect a U.S. Senator for how many years?', ['six (6)']),
+    new Question('Who is one of your state\'s U.S. Senators now?', ['Answers will vary. Find your Senator here.'], 'https://www.senate.gov/senators/senators-contact.htm'),
+    new Question('The House of Representatives has how many voting members?', ['four hundred thirty-five (435)']),
+    new Question('We elect a U.S. Representative for how many years?', ['two (2)']),
+    new Question('Name your U.S. Representative.', ['Answers will vary. Find your Representative here.'], 'https://www.house.gov/representatives/find-your-representative'),
+    new Question('Who does a U.S. Senator represent?', ['all people of the state']),
+    new Question('Why do some states have more Representatives than other states?', [
+        '(because of) the state\'s population',
+        '(because) they have more people',
+        '(because) some states have more people'
+    ]),
+    new Question('We elect a President for how many years?', ['four (4)']),
+    new Question('', ['']),
+    new Question('', ['']),
+    new Question('', ['']),
+    new Question('', ['']),
+    new Question('', ['']),
+    new Question('', ['']),
+    new Question('', ['']),
+    new Question('', ['']),
+    new Question('', ['']),
+    new Question('', ['']),
+    new Question('', ['']),
+    new Question('', ['']),
+    new Question('', ['']),
 ]
